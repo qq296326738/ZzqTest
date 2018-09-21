68ec617f134850b60fd5e027979c2896929ec510
qq296326738 <296326738@qq.com>
2018-09-21 16:31:34 +0800

    修改了配置文件名字

--- zzqSpringCloud/src/main/java/com/zzq/cloud/zzqcloud/ZzqcloudApplication.java
+++ zzqSpringCloud/src/main/java/com/zzq/cloud/zzqcloud/ZzqcloudApplication.java
@@ -10,6 +10,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class ZzqcloudApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ZzqcloudApplication.class, args);
     }
+
+    /*#打包
+mvn clean package
+# 分别以peer1和peeer2 配置信息启动eureka
+java -jar spring-cloud-eureka-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer1
+java -jar spring-cloud-eureka-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer2*/
 }
